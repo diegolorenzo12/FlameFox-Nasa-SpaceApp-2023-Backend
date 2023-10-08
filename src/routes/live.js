d188d832b123c9a9c67673c490caa6f8b467f63f
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-10-08 12:00:03 -0700

    Add parametric resource limit to the Live data endpoint

--- src/routes/live.js
+++ src/routes/live.js
@@ -4,18 +4,25 @@ const webserviceCfg = require('../configs/ws')
 
 liveRouter.get('/', async (req, res, next)=>{
     try{
+        //Filters
         const body = req.body
-        const page = typeof(req.query.page) !== 'undefined' ? req.query.page : 0
+        const page = typeof(req.query.page) !== 'undefined' ? Number(req.query.page) : 0
+        const resourceLimit = typeof(req.query.limit) !== 'undefined' ? Number(req.query.limit) : webserviceCfg.RESOURCE_PER_PAGE
+        
         const fireData = await fetchFireData('world')
-        const lowerSliceBound = page*webserviceCfg.RESOURCE_PER_PAGE
-        const upperSliceBound = lowerSliceBound +  webserviceCfg.RESOURCE_PER_PAGE
-        res.append("X-Total-Pages", Math.ceil(fireData.length/webserviceCfg.RESOURCE_PER_PAGE))
+        const lowerSliceBound = page * resourceLimit
+        const upperSliceBound = lowerSliceBound +  resourceLimit
+        console.log(`lower: ${lowerSliceBound}, upper: ${upperSliceBound}`)
+
+        res.append("X-Total-Pages", Math.ceil(fireData.length/resourceLimit))
         res.append("X-Total-Records", fireData.length)
         
         if(lowerSliceBound < fireData.length){
             return res.json(fireData.slice(lowerSliceBound, upperSliceBound))
         }else{
-            return res.json(fireData.slice(-webserviceCfg.RESOURCE_PER_PAGE))
+            //Prevents getting an empty response when requesting
+            //a page number higher than the total pages
+            return res.json(fireData.slice(-resourceLimit))
         }
 
     }catch(err){
