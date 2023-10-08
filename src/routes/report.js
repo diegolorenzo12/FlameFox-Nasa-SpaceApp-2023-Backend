0915ed8d7ab47d395d60940353776094c9671802
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-10-08 13:14:52 -0700

    added report by id

--- src/routes/report.js
+++ src/routes/report.js
@@ -3,88 +3,102 @@ const Report = require("../models/report");
 const multer = require("multer");
 const containerClient = require("../utils/containerClient");
 const { v4: uuidv4 } = require("uuid");
-const webserviceCfg = require("../configs/ws")
+const webserviceCfg = require("../configs/ws");
 
-reportRouter.get("/", async (req, res, next) =>{
-    const page = typeof(req.query.page) !== 'undefined' ? req.query.page : 0
-    
-    //If no confidence filter is specified, we ignore the confidence score alltogether
-    const minConfidence = typeof(req.query.confidence) !== 'undefined' ? req.query.confidence : 0
-    const body = req.body;
-    
-    const reports = await Report.find({
-        location:{
-            $near : {
-                $geometry:{
-                    type: "Point",
-                    coordinates: [body.longitude, body.latitude]
-                }
-            }
-        }
-    })
+reportRouter.get("/", async (req, res, next) => {
+  const page = typeof req.query.page !== "undefined" ? req.query.page : 0;
 
-    //This doesn't work
-    //reports = await reports.find({confidenceScore: {$gte: minConfidence}})
+  //If no confidence filter is specified, we ignore the confidence score alltogether
+  const minConfidence =
+    typeof req.query.confidence !== "undefined" ? req.query.confidence : 0;
+  const body = req.body;
 
+  const reports = await Report.find({
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [body.longitude, body.latitude],
+        },
+      },
+    },
+  });
 
-    //SQL ""Equivalent""":
-    //select * from reports where confidence > minConfidence and nearest(location) order by location asc
+  //This doesn't work
+  //reports = await reports.find({confidenceScore: {$gte: minConfidence}})
 
-    const totalPages = Math.ceil(reports.length / webserviceCfg.RESOURCE_PER_PAGE)
-    res.set('X-Total-Pages', totalPages)
-    res.set('X-Total-Records', reports.length)
-    
-    const lowerSliceBound = page*webserviceCfg.RESOURCE_PER_PAGE
-    const upperSliceBound = lowerSliceBound +  webserviceCfg.RESOURCE_PER_PAGE
-    
-    if(lowerSliceBound < reports.length){
-        return res.json(reports.slice(lowerSliceBound, upperSliceBound))
-    }else{
-        return res.json(reports.slice(-webserviceCfg.RESOURCE_PER_PAGE))
-    }
+  //SQL ""Equivalent""":
+  //select * from reports where confidence > minConfidence and nearest(location) order by location asc
 
+  const totalPages = Math.ceil(
+    reports.length / webserviceCfg.RESOURCE_PER_PAGE
+  );
+  res.set("X-Total-Pages", totalPages);
+  res.set("X-Total-Records", reports.length);
 
-})
+  const lowerSliceBound = page * webserviceCfg.RESOURCE_PER_PAGE;
+  const upperSliceBound = lowerSliceBound + webserviceCfg.RESOURCE_PER_PAGE;
+
+  if (lowerSliceBound < reports.length) {
+    return res.json(reports.slice(lowerSliceBound, upperSliceBound));
+  } else {
+    return res.json(reports.slice(-webserviceCfg.RESOURCE_PER_PAGE));
+  }
+});
 
 reportRouter.post("/", async (req, res, next) => {
-    
-    try{
-        const body = req.body;
-        
-        //Upload to azure blob storage
-        const blobName = `${Date.now()}-${uuidv4()}-${body.imageName}`;
-        const imgBuffer = Buffer.from(body.imageData, "base64")
-        
-        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-        const uploadResponse = await blockBlobClient.upload(imgBuffer,imgBuffer.length)
-            
-        //Check if the upload succesfull
-        if (!uploadResponse.requestId || uploadResponse.error) {
-            console.error("Error uploading file:", uploadResponse);
-            return res.status(400).json({ error: "Blob upload error" });
-        }
-        const imageUrl = blockBlobClient.url;
-        console.log(imageUrl);
-            
-            // Create a new report
-            const report = new Report({
-                location: {
-                    type: "Point",
-                    coordinates: [ body.longitude, body.latitude]
-                },
-                imageUrl: imageUrl,
-                imageName: blobName,
-                confidenceScore: 0.0,
-                brightness: 1.0,
-            });
-            
-            // Save the report to the database
-            await report.save();
-            res.status(201).json(report);
-        } catch (err) {
-            next(err)
-        }
+  try {
+    const body = req.body;
+
+    //Upload to azure blob storage
+    const blobName = `${Date.now()}-${uuidv4()}-${body.imageName}`;
+    const imgBuffer = Buffer.from(body.imageData, "base64");
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadResponse = await blockBlobClient.upload(
+      imgBuffer,
+      imgBuffer.length
+    );
+
+    //Check if the upload succesfull
+    if (!uploadResponse.requestId || uploadResponse.error) {
+      console.error("Error uploading file:", uploadResponse);
+      return res.status(400).json({ error: "Blob upload error" });
+    }
+    const imageUrl = blockBlobClient.url;
+    console.log(imageUrl);
+
+    // Create a new report
+    const report = new Report({
+      location: {
+        type: "Point",
+        coordinates: [body.longitude, body.latitude],
+      },
+      imageUrl: imageUrl,
+      imageName: blobName,
+      confidenceScore: 0.0,
+      brightness: 1.0,
     });
-    
-    module.exports = reportRouter;
-    
+
+    // Save the report to the database
+    await report.save();
+    res.status(201).json(report);
+  } catch (err) {
+    next(err);
+  }
+});
+
+reportRouter.get("/:id", async (req, res) => {
+  try {
+    const report = await Report.findById(req.params.id);
+    if (!report) {
+      return res.status(404).json({ message: "Report not found" });
+    }
+    res.json(report);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = reportRouter;
