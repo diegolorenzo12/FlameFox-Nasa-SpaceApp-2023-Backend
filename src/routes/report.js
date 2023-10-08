042a12e11284a2f377a38b804fe00866c2f5b84e
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-10-08 02:07:59 -0700

    implemented /report that stores images in azure blob storage

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,31 +1,58 @@
-const reportRouter = require('express').Router()
-const Report = require('../models/report')
+const reportRouter = require("express").Router();
+const Report = require("../models/report");
+const multer = require("multer");
+const containerClient = require("../configs/containerClient");
+const { v4: uuidv4 } = require("uuid");
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-reportRouter.get('/', async (req, res)=>{
-    res.send("Pong!")
-})
+reportRouter.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const longitude = req.body.longitude;
+    const latitude = req.body.latitude;
+    console.log(req.body);
 
-reportRouter.post('/', async (req, res, next)=>{
-    try{
-        const body = req.body;
-        const report = new Report({
-            longitude: body.longitude,
-            latitude: body.latitude,
-            images: [body.images], //TODO: Validate these images are actually from our CDN,
-            confidenceScore: 0.0,
-            brightness: 1.0
-        });
-        
-        await report.save()
-        return res.json({
-            status: "success",
-            id: note._id
-        })
-    }catch(err){
-        next(err)
+    if (!longitude || !latitude || !req.file) {
+      return res.status(400).json({
+        error: "Provide all parameters, longitude, latitude and file",
+      });
     }
-    
-})
 
-module.exports = reportRouter
+    //upload to azure blob storage
+    const blobName = `${Date.now()}-${uuidv4()}-${req.file.originalname}`;
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadResponse = await blockBlobClient.upload(
+      req.file.buffer,
+      req.file.buffer.length
+    );
+
+    //check is upload succesfull
+    if (!uploadResponse.requestId && uploadResponse.error) {
+      console.error("Error uploading file:", uploadResponse);
+      return res.status(400).json({ error: "Connection error" });
+    }
+    const imageUrl = blockBlobClient.url;
+    console.log(imageUrl);
+
+    // Create a new report
+    const report = new Report({
+      longitude: req.body.longitude,
+      latitude: req.body.latitude,
+      imageUrl: imageUrl,
+      imageName: blobName,
+      confidenceScore: 0.0,
+      brightness: 1.0,
+    });
+
+    // Save the report to the database
+    await report.save();
+
+    res.status(201).json(report);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to add report" });
+  }
+});
+
+module.exports = reportRouter;
