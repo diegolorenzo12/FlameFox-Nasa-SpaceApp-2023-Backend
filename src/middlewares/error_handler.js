d1f4f42cd8f5be33dfea784bf57a3dbe0a053b23
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-10-08 13:47:59 -0700

    Handle AxiosError exceptions

--- src/middlewares/error_handler.js
+++ src/middlewares/error_handler.js
@@ -1,11 +1,20 @@
 const errorHandler = (err, req, res, next) =>{
     console.error(err.message)
+    
+    //Error 400-499
     if(err.name === 'ValidationError'){
         return res.status(400).json({
             error: err.name,
             details: err.message
         })
-    }else if(err.name === 'AxiosError'){
+    }else if(err.name == 'CastError'){
+        return res.status(400).json({
+            error: err.name,
+            details: err.message
+        })
+    }
+    //Error 500-599
+    else if(err.name === 'AxiosError'){
         return res.status(500).json({
             error: err.name,
             details: err.message
