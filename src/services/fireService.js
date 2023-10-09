9c18cba4e40ac6818550f2aca377e874f3619c3e
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-10-08 23:12:28 -0700

    Change instrument to fire mapping to  MODIS

--- src/services/fireService.js
+++ src/services/fireService.js
@@ -8,7 +8,7 @@ const fetchFireData = async (area) =>{
     cachedData = appCache.get(cacheKey)
     if(cachedData == undefined){
         console.log("Cache miss. Requesting fresh data from FIRMS")
-        const raw = await axios.get(`https://firms.modaps.eosdis.nasa.gov/api/area/csv/${apiCfg.FIRMS_KEY}/VIIRS_SNPP_NRT/${area}/1`)
+        const raw = await axios.get(`https://firms.modaps.eosdis.nasa.gov/api/area/csv/${apiCfg.FIRMS_KEY}/MODIS_NRT/${area}/1`)
         const json = await converter().fromString(raw.data)
         appCache.set(cacheKey, json)
         return json;
